3af9056935a830f6daff2e53829a98a62dc01a47
Anton <36937480+korelskiy@users.noreply.github.com>
2021-05-17 11:36:23 +0300

    Add files via upload

---
+++ js/nav.js
@@ -0,0 +1,1 @@
+const burger=document.querySelector(".page-header__burger"),main_nav=document.querySelector(".main-nav");main_nav.classList.remove("main-nav--nojs"),burger.classList.remove("page-header__burger--nojs"),main_nav.classList.add("main-nav--closed"),burger.onclick=function(){main_nav.classList.toggle("main-nav--closed"),burger.classList.toggle("page-header__burger--closed")};
